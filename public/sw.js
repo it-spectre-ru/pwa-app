d90b97ed010cf54fac4398ea8f4713714596e7d0
it-spetre <it.spectre.ru@gmail.com>
2018-10-24 17:53:38 +0500

    non-lifecycle events

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating service worker ...', event);
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', function (event) {
+    console.log('[Service Worker] Fetching something ...', event);
+    event.respondWith(fetch(event.request));
+});
